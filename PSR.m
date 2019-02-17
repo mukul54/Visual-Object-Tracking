d9db351b5d8407f3a0c750aca8cca644ccc6d3ac
Mukul Ranjan <mukulranjaniitg@gmail.com>
2019-02-17 12:58:33 +0530

    Delete PSR.m

--- PSR.m
+++
@@ -1,6 +0,0 @@
-function psr = PSR( response )
-    maxresponse = max(response(:));
-    m = sum(response(:))/numel(response);
-    d = std(response(:));
-    psr =( (maxresponse - m)/d )^2;    
-end
